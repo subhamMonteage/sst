d153dedfb11a94f65fbf94794dc2e44cb7b7495d
Frank <frank@sst.dev>
2025-03-03 20:02:28 -0500

    Examples: add Astro v5 static mode to playground

---
+++ examples/internal/playground/sites/astro5-static/astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+//import aws from "astro-sst";
+import aws from "../../../../../../astro-sst/packages/astro-sst/dist/adapter";
+
+// https://astro.build/config
+export default defineConfig({
+  image: {
+    domains: ["sst.dev"],
+  },
+  output: "static",
+  adapter: aws(),
+});
