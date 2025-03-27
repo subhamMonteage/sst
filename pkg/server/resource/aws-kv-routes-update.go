d4c65da34079fbc9dc13087400922b5dc9e6cd56
Frank <frank@sst.dev>
2025-03-27 12:49:42 -0400

    Router: fix raise condition adding multiple routes

--- pkg/server/resource/aws-kv-routes-update.go
+++ pkg/server/resource/aws-kv-routes-update.go
@@ -31,145 +31,17 @@ type KvRoutesUpdateOutputs struct {
 	Namespace string   `json:"namespace,omitempty"`
 }
 
-func (r *KvRoutesUpdate) getKVClient() (*cloudfrontkeyvaluestore.Client, error) {
+func (r *KvRoutesUpdate) Create(input *KvRoutesUpdateInputs, output *CreateResult[KvRoutesUpdateOutputs]) error {
+	// get client
 	cfg, err := r.config()
 	if err != nil {
-		return nil, err
-	}
-	return cloudfrontkeyvaluestore.NewFromConfig(cfg), nil
-}
-
-func (r *KvRoutesUpdate) getRoutes(client *cloudfrontkeyvaluestore.Client, storeARN, key string) ([]string, error) {
-	getResp, err := client.GetKey(r.context, &cloudfrontkeyvaluestore.GetKeyInput{
-		KvsARN: aws.String(storeARN),
-		Key:    aws.String(key),
-	})
-	
-	if err != nil {
-		var notFoundErr *types.ResourceNotFoundException
-		if errors.As(err, &notFoundErr) {
-			// Key not found, return empty entries
-			return []string{}, nil
-		}
-		// Other error, propagate
-		return nil, err
-	}
-	
-	if getResp.Value == nil {
-		// No value
-		return []string{}, nil
-	}
-	
-	// Parse the array
-	var entries []string
-	if err := json.Unmarshal([]byte(*getResp.Value), &entries); err != nil {
-		return nil, fmt.Errorf("failed to unmarshal existing entries: %w", err)
-	}
-	
-	return entries, nil
-}
-
-func (r *KvRoutesUpdate) setRoutes(client *cloudfrontkeyvaluestore.Client, storeARN, key string, entries []string) error {
-	entriesJSON, err := json.Marshal(entries)
-	if err != nil {
-		return fmt.Errorf("failed to marshal entries: %w", err)
-	}
-	entriesStr := string(entriesJSON)
-
-	// get etag
-	descResp, err := client.DescribeKeyValueStore(r.context, &cloudfrontkeyvaluestore.DescribeKeyValueStoreInput{
-		KvsARN: aws.String(storeARN),
-	})
-	if err != nil {
 		return err
 	}
-
-	// update
-	_, err = client.UpdateKeys(r.context, &cloudfrontkeyvaluestore.UpdateKeysInput{
-		KvsARN:  aws.String(storeARN),
-		IfMatch: descResp.ETag,
-		Puts: []types.PutKeyRequestListItem{
-			{
-				Key:   aws.String(key),
-				Value: aws.String(entriesStr),
-			},
-		},
-	})
-
-	// retry if etag pre-condition failed
-	if err != nil {
-		var preconditionFailedErr *types.ValidationException
-		if errors.As(err, &preconditionFailedErr) && preconditionFailedErr.Message != nil && strings.Contains(*preconditionFailedErr.Message, "Pre-Condition failed") {
-			// sleep for a random time between 100 and 500ms
-			time.Sleep(time.Duration(rand.Intn(400)+100) * time.Millisecond)
-			return r.setRoutes(client, storeARN, key, entries)
-		}
-	}
-
-	return err
-}
-
-func existsRoute(entries []string, entry string) bool {
-	for _, e := range entries {
-		if e == entry {
-			return true
-		}
-	}
-	return false
-}
-
-func removeRoute(entries []string, entryToRemove string) []string {
-	var result []string
-	for _, v := range entries {
-		if v != entryToRemove {
-			result = append(result, v)
-		}
-	}
-	return result
-}
-
-func (r *KvRoutesUpdate) deleteKey(client *cloudfrontkeyvaluestore.Client, storeARN, key string) error {
-	// get etag
-	descResp, err := client.DescribeKeyValueStore(r.context, &cloudfrontkeyvaluestore.DescribeKeyValueStoreInput{
-		KvsARN: aws.String(storeARN),
-	})
-	if err != nil {
-		return err
-	}
-
-	// delete
-	_, err = client.UpdateKeys(r.context, &cloudfrontkeyvaluestore.UpdateKeysInput{
-		KvsARN:  aws.String(storeARN),
-		IfMatch: descResp.ETag,
-		Deletes: []types.DeleteKeyRequestListItem{
-			{
-				Key: aws.String(key),
-			},
-		},
-	})
-
-	// retry if etag pre-condition failed
-	if err != nil {
-		var preconditionFailedErr *types.ValidationException
-		if errors.As(err, &preconditionFailedErr) && preconditionFailedErr.Message != nil && strings.Contains(*preconditionFailedErr.Message, "Pre-Condition failed") {
-			// sleep for a random time between 100 and 500ms
-			time.Sleep(time.Duration(rand.Intn(400)+100) * time.Millisecond)
-			return r.deleteKey(client, storeARN, key)
-		}
-	}
-
-	return err
-}
-
-func (r *KvRoutesUpdate) Create(input *KvRoutesUpdateInputs, output *CreateResult[KvRoutesUpdateOutputs]) error {
-	client, err := r.getKVClient()
-	if err != nil {
-		return err
-	}
+	client := cloudfrontkeyvaluestore.NewFromConfig(cfg)
 
 	// Get all routes
 	fullKey := input.Namespace + ":" + input.Key
-	existingRoutes, err := r.getRoutes(client, input.Store, fullKey)
+	existingRoutes, etag, err := r.getRoutesWithEtag(client, input.Store, fullKey)
 	if err != nil {
 		return err
 	}
@@ -177,8 +49,15 @@ func (r *KvRoutesUpdate) Create(input *KvRoutesUpdateInputs, output *CreateResul
 	// Append route if not exists
 	if !existsRoute(existingRoutes, input.Entry) {
 		routes := append(existingRoutes, input.Entry)
-		err = r.setRoutes(client, input.Store, fullKey, routes)
+		err = r.setRoutes(client, input.Store, etag, fullKey, routes)
+		// retry if etag pre-condition failed
 		if err != nil {
+			var preconditionFailedErr *types.ValidationException
+			if errors.As(err, &preconditionFailedErr) && preconditionFailedErr.Message != nil && strings.Contains(*preconditionFailedErr.Message, "Pre-Condition failed") {
+				// sleep for a random time between 100 and 500ms
+				time.Sleep(time.Duration(rand.Intn(400)+100) * time.Millisecond)
+				return r.Create(input, output)
+			}
 			return err
 		}
 	}
@@ -228,15 +107,16 @@ func (r *KvRoutesUpdate) Update(input *UpdateInput[KvRoutesUpdateInputs, KvRoute
 		return nil
 	}
 	
-	// Only the entry has changed, but we want to replace, not just append
-	client, err := r.getKVClient()
+	// get client
+	cfg, err := r.config()
 	if err != nil {
 		return err
 	}
+	client := cloudfrontkeyvaluestore.NewFromConfig(cfg)
 
 	// Get all routes
 	fullKey := input.News.Namespace + ":" + input.News.Key
-	existingEntries, err := r.getRoutes(client, input.News.Store, fullKey)
+	existingEntries, etag, err := r.getRoutesWithEtag(client, input.News.Store, fullKey)
 	if err != nil {
 		return err
 	}
@@ -248,8 +128,14 @@ func (r *KvRoutesUpdate) Update(input *UpdateInput[KvRoutesUpdateInputs, KvRoute
 	}
 	
 	// Save routes
-	err = r.setRoutes(client, input.News.Store, fullKey, entries)
+	err = r.setRoutes(client, input.News.Store, etag, fullKey, entries)
 	if err != nil {
+		var preconditionFailedErr *types.ValidationException
+		if errors.As(err, &preconditionFailedErr) && preconditionFailedErr.Message != nil && strings.Contains(*preconditionFailedErr.Message, "Pre-Condition failed") {
+			// sleep for a random time between 100 and 500ms
+			time.Sleep(time.Duration(rand.Intn(400)+100) * time.Millisecond)
+			return r.Update(input, output)
+		}
 		return err
 	}
 	
@@ -265,20 +151,17 @@ func (r *KvRoutesUpdate) Update(input *UpdateInput[KvRoutesUpdateInputs, KvRoute
 }
 
 func (r *KvRoutesUpdate) Delete(input *DeleteInput[KvRoutesUpdateOutputs], output *int) error {
-	client, err := r.getKVClient()
+	// get client
+	cfg, err := r.config()
 	if err != nil {
 		return err
 	}
+	client := cloudfrontkeyvaluestore.NewFromConfig(cfg)
 
 	// Get all routes
 	fullKey := input.Outs.Namespace + ":" + input.Outs.Key
-	existingRoutes, err := r.getRoutes(client, input.Outs.Store, fullKey)
+	existingRoutes, etag, err := r.getRoutesWithEtag(client, input.Outs.Store, fullKey)
 	if err != nil {
-		var notFoundErr *types.ResourceNotFoundException
-		if errors.As(err, &notFoundErr) {
-			// Key not found, nothing to do
-			return nil
-		}
 		return err
 	}
 
@@ -291,8 +174,122 @@ func (r *KvRoutesUpdate) Delete(input *DeleteInput[KvRoutesUpdateOutputs], outpu
 	}
 
 	if len(routes) == 0 {
-		return r.deleteKey(client, input.Outs.Store, fullKey)
+		err = r.deleteKey(client, input.Outs.Store, etag, fullKey)
 	} else {
-		return r.setRoutes(client, input.Outs.Store, fullKey, routes)
+		err = r.setRoutes(client, input.Outs.Store, etag, fullKey, routes)
 	}
+	if err != nil {
+		var preconditionFailedErr *types.ValidationException
+		if errors.As(err, &preconditionFailedErr) && preconditionFailedErr.Message != nil && strings.Contains(*preconditionFailedErr.Message, "Pre-Condition failed") {
+			// sleep for a random time between 100 and 500ms
+			time.Sleep(time.Duration(rand.Intn(400)+100) * time.Millisecond)
+			return r.Delete(input, output)
+		}
+		return err
+	}
+
+	return nil
+}
+
+func (r *KvRoutesUpdate) getRoutesWithEtag(client *cloudfrontkeyvaluestore.Client, storeARN, key string) ([]string, string, error) {
+	// get etag
+	descResp, err := client.DescribeKeyValueStore(r.context, &cloudfrontkeyvaluestore.DescribeKeyValueStoreInput{
+		KvsARN: aws.String(storeARN),
+	})
+	if err != nil {
+		return nil, "", err
+	}
+	etag := *descResp.ETag
+
+	// get routes
+	getResp, err := client.GetKey(r.context, &cloudfrontkeyvaluestore.GetKeyInput{
+		KvsARN: aws.String(storeARN),
+		Key:    aws.String(key),
+	})
+	
+	if err != nil {
+		var notFoundErr *types.ResourceNotFoundException
+		if errors.As(err, &notFoundErr) {
+			// Key not found, return empty entries
+			return []string{}, etag, nil
+		}
+		// other error, propagate
+		return nil, etag, err
+	}
+	
+	if getResp.Value == nil {
+		// no value
+		return []string{}, etag, nil
+	}
+	
+	// Parse routes array
+	var entries []string
+	if err := json.Unmarshal([]byte(*getResp.Value), &entries); err != nil {
+		return nil, etag, fmt.Errorf("failed to unmarshal existing entries: %w", err)
+	}
+	
+	return entries, etag, nil
+}
+
+func (r *KvRoutesUpdate) setRoutes(client *cloudfrontkeyvaluestore.Client, storeARN string, etag string, key string, entries []string) error {
+	entriesJSON, err := json.Marshal(entries)
+	if err != nil {
+		return fmt.Errorf("failed to marshal entries: %w", err)
+	}
+	entriesStr := string(entriesJSON)
+
+	// update
+	_, err = client.UpdateKeys(r.context, &cloudfrontkeyvaluestore.UpdateKeysInput{
+		KvsARN:  aws.String(storeARN),
+		IfMatch: aws.String(etag),
+		Puts: []types.PutKeyRequestListItem{
+			{
+				Key:   aws.String(key),
+				Value: aws.String(entriesStr),
+			},
+		},
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *KvRoutesUpdate) deleteKey(client *cloudfrontkeyvaluestore.Client, storeARN, etag string, key string) error {
+	// delete
+	_, err := client.UpdateKeys(r.context, &cloudfrontkeyvaluestore.UpdateKeysInput{
+		KvsARN:  aws.String(storeARN),
+		IfMatch: aws.String(etag),
+		Deletes: []types.DeleteKeyRequestListItem{
+			{
+				Key: aws.String(key),
+			},
+		},
+	})
+
+	// retry if etag pre-condition failed
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func existsRoute(entries []string, entry string) bool {
+	for _, e := range entries {
+		if e == entry {
+			return true
+		}
+	}
+	return false
+}
+
+func removeRoute(entries []string, entryToRemove string) []string {
+	var result []string
+	for _, v := range entries {
+		if v != entryToRemove {
+			result = append(result, v)
+		}
+	}
+	return result
 }
