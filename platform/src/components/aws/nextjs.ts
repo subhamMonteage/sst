3f4a9f96946130af98274608a24301fc3658ffa4
Frank <frank@sst.dev>
2025-04-02 18:46:19 -0400

    Nextjs: default OpenNext version to 3.5.4 with security patch

--- platform/src/components/aws/nextjs.ts
+++ platform/src/components/aws/nextjs.ts
@@ -11,7 +11,7 @@ import { isALteB } from "../../util/compare-semver.js";
 import { Plan, SsrSite, SsrSiteArgs } from "./ssr-site.js";
 import { Bucket } from "./bucket.js";
 
-const DEFAULT_OPEN_NEXT_VERSION = "3.5.3";
+const DEFAULT_OPEN_NEXT_VERSION = "3.5.4";
 
 type BaseFunction = {
   handler: string;
